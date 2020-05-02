3019e228f029880e426cc15ea6f66f86c3b56492
stepanet <stepanet@gmail.com>
2020-05-02 22:37:03 +0300

    ver 1.0

--- lineChart/ViewController.swift
+++ lineChart/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lineChart: LineChart!
+    @IBOutlet weak var curvedlineChart: LineChart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.3529411765, blue: 0.6156862745, alpha: 1)
+        
+        // Sample dataset
+//        let dataEntries = [PointEntry(value: 0, title: ""), PointEntry(value: 100, title: ""), PointEntry(value: 100, title: ""), PointEntry(value: 100, title: ""), PointEntry(value: 20, title: ""), PointEntry(value: 30, title: ""), PointEntry(value: 120, title: "")]
+        
+        let dataEntries = generateRandomEntries()
+        print(dataEntries)
+        //lineChart.dataEntries = dataEntries
+        //lineChart.isCurved = false
+        
+        //curvedlineChart.dataEntries = dataEntries
+        //curvedlineChart.isCurved = true
     }
-
-
+    
+    private func generateRandomEntries() -> [PointEntry] {
+        var result: [PointEntry] = []
+        for i in 0..<10 {
+            let value = Int(arc4random() % 500)
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d MMM"
+            var date = Date()
+            date.addTimeInterval(TimeInterval(24*60*60*i))
+            
+            result.append(PointEntry(value: value, label: formatter.string(from: date)))
+        }
+        return result
+    }
 }
 
